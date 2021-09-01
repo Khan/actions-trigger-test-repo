3386c69f6b94b2cd8d87047f4f09f4cbb9740a9f
Lilli Szafranski <lilli@khanacademy.org>
2021-09-01 10:56:44 -0700

    [demo] Appending `34: HERMAN_HERMANN SARAH_WIGGUM` to foo

--- automate2.sh
+++ automate2.sh
@@ -24,7 +24,7 @@ LINE_NO=$(echo $LINE_NO | xargs)
 echo "Updating file..."
 echo "$LINE_NO: $LOREM1 $LOREM2" >> $FILE
 
-echo "Committing change..."
+echo "Committing change...\n"
 set -x
 git commit -am "Appending \`$LINE_NO: $LOREM1 $LOREM2\` to $FILE"
 
